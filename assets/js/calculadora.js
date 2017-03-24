44fd4a04cd9fcdeed859a97eb6b64ad43c237e63
sue martinez <sueherrera30@gmail.com>
2017-03-24 15:59:47 +0000

    calculadora con funcion de suma

--- assets/js/calculadora.js
+++ assets/js/calculadora.js
@@ -1,22 +1,39 @@
 
-var operadores=["+","-","*","/"];
+// var operadores=["+","-","*","/"];
 
 function mostrar(valor){
-var input = document.getElementById('resultado');
+var input = document.getElementById('operaciones');
 input.value += valor;
 }
 
 function limpiar() {
-var borrar = document.getElementById('resultado');
+var borrar = document.getElementById('operaciones');
  borrar.value = "";
  }
 
    function almacenar(){
-   var valor=document.getElementById("resultado").value;
-   var almacen = valor.split("+");// aqui planeaba poner la variable de operaciones para que agregara el simbolo correspondiente ;s
-   almacen.push("+");
-   console.log(almacen);
- }
+     var numeros =document.getElementById("operaciones").value;
+     var respuesta = document.getElementById('resultado');
+     var almacen = numeros.split("+");
+     var suma= Number(almacen[0]) + Number(almacen [1]);
+     respuesta.value = suma;
+
+        //  case resta:
+        //  var numeros =document.getElementById("operaciones").value;
+        //  var respuesta = document.getElementById('resultado');
+        // var almacen = numeros.split("-");
+        //  var resta= Number(almacen[0]) - Number(almacen [1]);
+        //  respuesta.value = resta;
+        //    break;
+
+
+
+
+
+
+   // aqui planeaba poner la variable de operaciones para que agregara el simbolo correspondiente ;s
+   //almacen.push("+");
+  }
 
  /*function operaciones(){
 
